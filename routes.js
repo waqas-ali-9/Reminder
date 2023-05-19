a2a5ce7f8a2efbe405a0c6417e3ea783d4794fb9
Waqas Shabbir <wkaxhank@gmail.com>
2023-05-19 23:52:13 +0500

    final commit

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 const Router = require('express');
 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 const reminder = require('./routes/reminder.js');
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 module.exports = Router()
     .use('/api/remindernotify',reminder)
--- routes/reminder.js
+++ routes/reminder.js
@@ -3,11 +3,13 @@ const router = Router();
 const db = require('../database.js');
 const email=require('../support/email.js');
 const schedule = require('node-schedule')
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 //Triggers everyday at 6 am.
 schedule.scheduleJob('0 0 6 * * ?', () => {
          sendReminder();
 })
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 //this function will get all the reminders that will be sent.
 async function sendReminder(){    
@@ -23,6 +25,8 @@ async function sendReminder(){
         sendEmailToAll(result[0][0]) 
     }
 }
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 //this functions sends the email to every reminder in the array passed
 //into this function
 function sendEmailToAll(data)
@@ -31,6 +35,7 @@ function sendEmailToAll(data)
         email.sendMail(element.email,element.subject,setMessage(element))
     });
 }
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 //this function gets the text message and applied html tags on it for 
 //formatting
